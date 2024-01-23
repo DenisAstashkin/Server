9f6e30c7a2c0f0becc582b1b5c491be4598e2375
DenisAstashkin <ast.den27@gmail.com>
2024-01-23 22:23:09 +0400

    Add log in code

--- Server.hpp
+++ Server.hpp
@@ -32,12 +32,22 @@ public:
     Server(int type_connection=SOCK_STREAM, int ip_version=AF_INET, int protocol=0, std::string ip="127.0.0.1",
            std::string port="8700", int max_client=SOMAXCONN, std::string path_log="unknown", std::string log_name="unknown", int max_threads=2)
     {
+        log = Log(path_log, log_name);
         serverSocket = socket(ip_version, type_connection, protocol);
+        if (serverSocket < 0)
+        {
+            log.write(std::string().append(ERROR).append("Socket don't open."));
+            exit(-1);
+        }
         serverAddress.sin_family = ip_version;
         serverAddress.sin_port = htons(atoi(port.c_str()));
         serverAddress.sin_addr.s_addr = inet_addr(ip.c_str());
         this->max_thread = max_thread;
-        bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress));
+        if (bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) < 0)
+        {
+            log.write(std::string().append(ERROR).append("Failed bind."));
+            exit(-1);
+        }
         if (max_client <= SOMAXCONN)
         {
             this->max_client = max_client;
@@ -50,12 +60,21 @@ public:
 
     void Start(void(*action)(int*))
     {
+        log.write(std::string().append(INFO).append("Server start..."));
         std::atomic_ulong count_thread = std::atomic_ulong(0);
+        if (listen(serverSocket, SOMAXCONN) < 0)
+        {
+            log.write(std::string().append(ERROR).append("Failed listen."));
+            exit(-1);
+        }
+        int clientSocket;
         for(;;)
         {
-            listen(serverSocket, SOMAXCONN);
-            int clientSocket;
-            clientSocket = accept(serverSocket, nullptr, nullptr);
+            if ((clientSocket = accept(serverSocket, nullptr, nullptr) < 0))
+            {
+                log.write(std::string().append(ERROR).append("Client try connected."));
+                exit(-1);
+            }
             if (FreeThread(&count_thread))
             {
                 std::thread([&action, &count_thread, &clientSocket](){
