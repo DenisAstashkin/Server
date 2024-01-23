9ec48805d65cca47b3ca367bef6a85cd2ad37315
DenisAstashkin <ast.den27@gmail.com>
2024-01-23 22:10:58 +0400

    Add constructor

--- Log.hpp
+++ Log.hpp
@@ -6,4 +6,10 @@ class Log
 private:
     std::string path;
     std::string name;
+public:
+    Log(std::string path="unknown", std::string name="unknown")
+    {
+        this->path = path;
+        this->name = name;
+    }
 };
